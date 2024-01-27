b97f075d8055b5cf5dd4ecfe56a9645a7a9dba81
Goutham Meka <gouthammeka15@gmail.com>
2024-01-27 16:13:04 +0900

    add line drawing

---
+++ GGJ-2024-RoughLaugh/Meka/StickProjector.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var leftHead : Node2D
+var rightHead : Node2D
+
+func _ready():
+	leftHead = $LeftChopstickHead
+	rightHead = $RightChopstickHead
+	
+func _process(delta):
+	queue_redraw()# Ensure that _draw() is called on every frame
+
+func _draw():
+	var leftHeadCenter = (leftHead.global_position)
+	var rightHeadCenter = (rightHead.global_position)
+
+	var p1 = Vector2(120, 600)
+	var p2 = Vector2(1080, 600)
+	
+	print(leftHeadCenter)
+	print(rightHeadCenter)
+	draw_line(leftHeadCenter, p1, Color(1, 1, 1), 20)
+	draw_line(rightHeadCenter, p2, Color(1, 1, 1), 20)
+#
+#
+#func update_line(point1, point2):
+	#draw_line(point1, point2, Color(1, 1, 1), 5)
+
