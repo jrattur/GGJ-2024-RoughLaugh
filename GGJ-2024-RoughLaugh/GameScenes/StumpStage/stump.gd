09da484009843890a8fbb344f4947c0414cd1ceb
gomaoil <foluken@gmail.com>
2024-01-27 21:09:09 +0900

    Craate stump stage and make it work.

---
+++ GGJ-2024-RoughLaugh/GameScenes/StumpStage/stump.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+var isInked = false
+var redHead: Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	redHead = $Stump/RedHead;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_area_2d_body_entered(body: PhysicsBody2D):
+	isInked = true;
+	redHead.visible = true;
+
+
