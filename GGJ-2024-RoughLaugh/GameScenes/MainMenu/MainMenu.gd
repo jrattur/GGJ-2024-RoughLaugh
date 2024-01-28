d205ff1ab46176db795cf1d36cd07b9d3f74ba8f
Goutham Meka <gouthammeka15@gmail.com>
2024-01-28 14:50:27 +0900

    update main menu

--- GGJ-2024-RoughLaugh/GameScenes/MainMenu/MainMenu.gd
+++ GGJ-2024-RoughLaugh/GameScenes/MainMenu/MainMenu.gd
@@ -1,12 +1,68 @@
 extends Node2D
 
+var playIdle: Sprite2D
+var playOnHover: Sprite2D
+var playOnPressed: Sprite2D
+var aboutIdle: Sprite2D
+var aboutOnHover: Sprite2D
+var aboutOnPressed: Sprite2D
 
 func _ready():
-	pass
+	playIdle = $PlayButton/PlayIdle
+	playOnHover = $PlayButton/PlayOnHover
+	playOnPressed = $PlayButton/PlayOnPressed
+	aboutIdle = $AboutButton/AboutIdle
+	aboutOnHover = $AboutButton/AboutOnHover
+	aboutOnPressed = $AboutButton/AboutOnPressed
+	playIdle.visible = true
+	playOnHover.visible = false
+	playOnPressed.visible = false
+	aboutIdle.visible = true
+	aboutOnHover.visible = false
+	aboutOnPressed.visible = false
 
 func _process(delta):
 	pass
 
-func _on_play_button_pressed():
+func _on_play_button_button_up():
 	get_tree().change_scene_to_file("res://GameScenes/StoryFrames/StoryFrame1.tscn")
 
+
+func _on_play_button_mouse_entered():
+	playIdle.visible = false
+	playOnHover.visible = true
+	playOnPressed.visible = false
+
+
+func _on_play_button_mouse_exited():
+	playIdle.visible = true
+	playOnHover.visible = false
+	playOnPressed.visible = false
+
+
+func _on_play_button_button_down():
+	playIdle.visible = false
+	playOnHover.visible = false
+	playOnPressed.visible = true
+
+
+func _on_about_button_button_down():
+	aboutIdle.visible = false
+	aboutOnHover.visible = false
+	aboutOnPressed.visible = true
+
+
+func _on_about_button_button_up():
+	pass # Replace with function body.
+
+
+func _on_about_button_mouse_entered():
+	aboutIdle.visible = false
+	aboutOnHover.visible = true
+	aboutOnPressed.visible = false
+
+
+func _on_about_button_mouse_exited():
+	aboutIdle.visible = true
+	aboutOnHover.visible = false
+	aboutOnPressed.visible = false
