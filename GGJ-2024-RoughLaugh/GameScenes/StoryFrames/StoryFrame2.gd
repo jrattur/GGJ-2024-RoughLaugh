d69913db19f026a2058fdded1ece4fbedf0dd323
Goutham Meka <gouthammeka15@gmail.com>
2024-01-28 10:23:52 +0900

    cleanup storyframe bugs

--- GGJ-2024-RoughLaugh/GameScenes/StoryFrames/StoryFrame2.gd
+++ GGJ-2024-RoughLaugh/GameScenes/StoryFrames/StoryFrame2.gd
@@ -3,14 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file("res://GameScenes/StumpStage/StumpStage.tscn")
  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://GameScenes/MainMenu/MainMenu.tscn")
